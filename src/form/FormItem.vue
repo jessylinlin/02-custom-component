4fa2eaee31b3494a7c89364360c3e4fcc4898aac
jessylinlin <1501103995@qq.com>
2021-08-11 19:16:41 +0800

    vue component

---
+++ src/form/FormItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <label>{{label}}</label>
+    <div>
+      <slot></slot>
+      <p v-if="errorMessage">{{errorMessage}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LgFormItem',
+  props: {
+    label: {
+      type: String,
+    },
+    prop: {
+      type: String,
+    }
+  },
+  data() {
+    return {
+      errorMessage: ''
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
